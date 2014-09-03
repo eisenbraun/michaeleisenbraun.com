71133bd0fbd12dc507565bcf66277dae4b19699a
eisenbraun <krepta00@gmail.com>
2014-09-03 00:16:48 -0400

    Update styles, scripting, and content

--- src/js/site.js
+++ src/js/site.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
+    var intro = document.getElementById('intro').getBoundingClientRect();
     var projects = document.getElementById('projects').getBoundingClientRect();
     var profile = document.getElementById('profile').getBoundingClientRect();
+    var contact = document.getElementById('contact').getBoundingClientRect();
 
     $(window).scroll(function() {
-        if($(window).scrollTop() >= profile.top) { 
+        if($(window).scrollTop() >= contact.top - 50) {
+            $('#nav').attr('class', 'gray');
+        } else if($(window).scrollTop() >= profile.top) {
+             $('#nav').attr('class', 'blue blue-bg');
+        } else if($(window).scrollTop() >= profile.top - 50) {
             $('#nav').attr('class', 'blue');
-        } else if($(window).scrollTop() >= projects.top) { 
+        } else if($(window).scrollTop() >= projects.top) {
+            $('#nav').attr('class', 'white white-bg');
+        } else if($(window).scrollTop() >= projects.top - 50) {
             $('#nav').attr('class', 'white');
-        } else { 
+        } else if($(window).scrollTop() <= projects.top - 50) {
             $('#nav').attr('class', 'red');
         }
-
-
     });
 
     $('section[data-type="background"]').each(function(){
@@ -19,13 +25,14 @@ $(document).ready(function(){
         var top = $('article', $bgobj).css('top');
 
         $(window).scroll(function() {
-            var yPos = 200 - ($(window).scrollTop() / $bgobj.data('speed')); 
+            var article = 300 - ($(window).scrollTop() / 3);
+            var aside = 300 - ($(window).scrollTop() / 4);
              
             // Put together our final background position
             var coords = '50% '+ yPos + 'px';
 
-            $('article', $bgobj).css({'top': yPos + 'px'});
- 
+            $('h1', $bgobj).css({'top': article + 'px'});
+            $('aside', $bgobj).css({'top': aside + 'px'});
             // Move the background
             //$bgobj.css({ 'background-position': coords });
         }); 
